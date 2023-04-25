50e08487a772e34cfd331473e8daaef80d7aa600
Beg <a.behaghel@inato.com>
2023-04-25 23:32:41 +0200

    ✨ Play with schema

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
+import * as Either from "@effect/data/Either";
+import { pipe } from "@effect/data/Function";
+import * as Pretty from "@effect/schema/Pretty";
+import * as Schema from "@effect/schema/Schema";
+import * as TreeFormatter from "@effect/schema/TreeFormatter";
+
+const Person = Schema.struct({
+  name: Schema.string,
+  age: Schema.number,
+});
+type Person = Schema.To<typeof Person>;
+
+const person = Schema.parseEither(Person)(
+  { blob: "", name: "haha", age: 12 },
+  { errors: "first", onExcessProperty: "ignore" }
+);
+
 const main = () => {
   console.log("main");
+  console.log(Pretty.from(Person)({ name: "", age: 12 }));
+  pipe(
+    person,
+    Either.mapLeft((e) => {
+      console.log(TreeFormatter.formatErrors(e.errors));
+      return e;
+    })
+  );
 };
 
 main();
